9a8fa748df8864b62266a150dc48218b9960ca12
mporo <mporokhovskyy2000@gmail.com>
2024-09-17 15:54:15 +0300

    add file

---
+++ lab1.py
@@ -0,0 +1,25 @@
+country = "Україна"
+print("Моя любима країна -",country)
+
+
+
+age = 17
+if age >= 35:
+    print("Ви можете стати президентом")
+else:
+    print("Ви замолодий щоб бути президентом")
+
+
+is_holiday = True
+is_weekend = False
+print(is_holiday)
+
+
+from math import sqrt, log
+
+x = 1.549
+y = 7.317
+
+result = 3.14*x**2 - 7*y**(-2) + x*y**3 + 7*sqrt(y) + log(x+7*sqrt(y))
+
+print(result)
