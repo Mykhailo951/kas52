a5f5c992637c35c16cb51514122caab896673f6c
Mykhailo951 <mporokhovskyy2000@gmail.com>
2024-10-03 19:07:03 +0300

    lab2.2.py

--- lab2.2.py
+++ lab2.2.py
@@ -1,33 +1,29 @@
-a = 1
-b = 1.5
-h = 0.05
+a, b, h = 1, 1.5, 0.05
+
 d = 10**(-5)
 
-
-def s_s(x, d):
+def sum(x, d):
     n = 1
     term = (x - 1)**n / (n * x**n)
-    s_s = term
+    sum = term
 
     while abs(term) > d:
         n += 1
         term = (x - 1)**n / (n * x**n)
-        s_s += term
-
-    return s_s
-
+        sum += term
+    return sum
 
 def tb(a, b, h, d):
     x = a
     res = []
 
     while x <= b:
-        y = s_s(x, d)
+        y = sum(x, d)
         res.append((x, y))
         x += h
-
+        x = round(x, 2)
+        print(x)
     return res
-
 
 res = tb(a, b, h, d)
 for x, y in res:
